451d73bc34adcd0ee0e624173fbd3b2dd7c20ff4
望月翔 <mochidzukishou@mochidzukishounoMacBook-Pro.local>
2023-10-24 19:45:14 +0900

    first commit

---
+++ sections/index.js
@@ -0,0 +1,12 @@
+import About from "./About";
+import Explore from "./Explore";
+import ExploreCard from "./ExploreCard";
+import Feedback from "./Feedback";
+import GetStarted from "./GetStarted";
+import Hero from "./Hero";
+import Insights from "./Insights";
+import WhatsNew from "./WhatsNew";
+import World from "./World";
+
+
+export {About, Explore, Feedback, GetStarted, Insights, WhatsNew, World, Hero, ExploreCard}
